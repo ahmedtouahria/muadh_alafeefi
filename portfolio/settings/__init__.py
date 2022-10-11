4c02c310b86e54e9b51304018fb4506dffb521b1
“ahmedtouahria” <ahmedtouahria2001@gmail.com>
2022-10-11 10:58:59 +0100

    update settings

--- portfolio/settings/__init__.py
+++ portfolio/settings/__init__.py
@@ -248,3 +248,20 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+SECRET_KEY = '-~aO;|9fJok;fopzF;rE[??/w^zcumh(9'
+DEBUG = False
+ALLOWED_HOSTS = ['muadhalafeefi.com','www.muadhalafeefi.com']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql', # on utilise l'adaptateur po>
+        'NAME': 'muadh', # le nom de notre base de données créée précédemment
+        'USER': 'ahmed', # attention : remplacez par votre nom d'utilisateur !!
+        'PASSWORD': 'SUDOahmed2001',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
